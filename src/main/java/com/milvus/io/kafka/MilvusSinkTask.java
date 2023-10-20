fa973931cbd6c8d6102e5decdc6637382bcf9633
Nian Liu <nian.liu@zilliz.com>
2023-10-20 10:10:52 +0800

    disable dynamic schema for now, we only support insert filed which clearly defined on Milvus collection

--- src/main/java/com/milvus/io/kafka/MilvusSinkTask.java
+++ src/main/java/com/milvus/io/kafka/MilvusSinkTask.java
@@ -84,22 +84,15 @@ public class MilvusSinkTask extends SinkTask {
     }
 
     private void WriteRecord(SinkRecord record, CollectionSchema collectionSchema) {
-        InsertParam insertParam;
-        if(collectionSchema.getEnableDynamicField()){
-            List<JSONObject> fields = converter.convertRecordWithDynamicSchema(record, collectionSchema);
-            insertParam = InsertParam.newBuilder()
-                    .withCollectionName(config.getCollectionName())
-                    .withRows(fields)
-                    .build();
-        }else {
-            List<InsertParam.Field> fields = converter.convertRecord(record, collectionSchema);
-            insertParam = InsertParam.newBuilder()
+        // not support dynamic schema for now, for dynamic schema, we need to put the data into a JSONObject
+        List<InsertParam.Field> fields = converter.convertRecord(record, collectionSchema);
+        InsertParam insertParam = InsertParam.newBuilder()
                     .withCollectionName(config.getCollectionName())
                     .withFields(fields)
                     .build();
-        }
-        log.debug("Inserting data to collection: " + config.getCollectionName() + " with fields: " +
-                insertParam.getFields() + "with rows: "+ insertParam.getRows());
+
+        log.info("Inserting data to collection: " + config.getCollectionName() + " with fields: " +
+                insertParam.getFields());
         myMilvusClient.insert(insertParam);
     }
 
